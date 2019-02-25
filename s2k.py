3886d622768f217a7ac42e3598954dd1b92dd8fa
awemous <dave@awemous.co.uk>
2019-02-25 17:28:24 +0000

    Add files via upload

---
+++ s2k.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+import os
+
+def convertCSV(filename):
+    with open(filename, "rt") as source, open("kash-" + os.path.basename(filename), "wt") as result:
+        rdr = csv.reader(source)
+        wtr = csv.writer(result, delimiter=',', )
+        next(rdr)  # Skip CSV headers
+        next(rdr)  # Skip "Opening balance."
+        wtr.writerow(['MoneyIn', 'MoneyOut', 'Date', 'Description', 'Reference']) # write header
+        for row in rdr:
+        	test = 0
+        	test = test + int(float(row[4]))
+        	if test < 0 : 
+        		wtr.writerow(['',row[4],row[0],row[1],row[2]])
+        	else:
+        		wtr.writerow([row[4],'',row[0],row[1],row[2]])
+
+def main(argv):
+    convertCSV(argv[0])
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
